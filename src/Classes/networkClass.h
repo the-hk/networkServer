dbf50ce83598e749b19a1d76ea1ab39a566766e8
the-hk <conjointchill@gmail.com>
2023-07-08 16:36:14 +0000

    first class is added

--- src/Classes/networkClass.h
+++ src/Classes/networkClass.h
@@ -1,4 +1,25 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
 
 #include<iostream>
 
-int sa();
+class networkClass{
+
+public:
+    int networkInit();
+    int readSocket();
+    int sendSocket();
+
+private:
+    int createSocket();
+    int bindNewConnection();
+    int listen();
+    int accept();
+};
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,5 @@ int main()
     	int a = 5;
         a = a * a;
 	std::cout<<a<<std::endl;
-        sa();
 	return 0;	
 }
